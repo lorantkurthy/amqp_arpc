bd86995baaf0afaebc358f7ebcf94677f9cac23a
Lorant Kurthy <kurthyl@gmail.com>
2014-12-07 18:14:43 +0100

    added start_link to worker behaviour

--- src/amqp_arpc_server_worker_behaviour.erl
+++ src/amqp_arpc_server_worker_behaviour.erl
@@ -1,3 +1,10 @@
 -module(amqp_arpc_server_worker_behaviour).
 
 -callback request(WorkerPid :: pid() , Payload :: binary(), ReplyFun :: fun((binary()) -> any() )) -> 'ok'.
+
+-callback start_link(WorkerArgs) -> {ok, Pid} |
+                                    {error, {already_started, Pid}} |
+                                    {error, Reason} when
+    WorkerArgs :: proplists:proplist(),
+    Pid        :: pid(),
+    Reason     :: term().
